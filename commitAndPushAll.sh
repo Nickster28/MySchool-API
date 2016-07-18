57ada526aa4dbaffb71ae5ecb9d8f10fb9174f8c
Nick Troccoli <nicktroccoli28@gmail.com>
2016-07-17 17:46:35 -0700

    Added script to commit and push to both staging and master

---
+++ commitAndPushAll.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git stash
+git checkout staging
+git stash apply
+echo -e "Enter commit message: "
+read
+git commit -a -m "$REPLY"
+git push origin staging
+git checkout master
+git push origin master
+git checkout staging
