6d4bad4a56290376545159a14bf542ba8439ecc6
Nick Troccoli <nicktroccoli28@gmail.com>
2016-08-12 00:38:13 -0400

    Added more error checking, debugged

--- cloud/editions.js
+++ cloud/editions.js
@@ -1,23 +1,26 @@
 /*
  * CLOUD FUNCTION: IsValidEditionName
  * ------------------------------------
- * Parameters:
+ * Request Parameters:
  *		name - the edition name to validate
  *
  * Returns: whether or not this edition name is valid (non-empty, unique).
+ *
+ * Must be called by a logged-in Parse User.
  * ------------------------------------
  */
 Parse.Cloud.define("IsValidEditionName", function(req, res) {
-	
-	// If there's no name, send an error, and if the name is empty, send false
-	if (!req.params.name) {
+	if (!req.user) {
+		res.error("Request must be made by logged-in user");
+	} else if (req.params.name === undefined || req.params.name === null) {
 		res.error("No name provided");
-	else if (req.params.name === "") {
+	} else if (req.params.name === "") {
 		res.success(false);
 	} else {
+		var token = req.user.getSessionToken();
 		var editionsQuery = new Parse.Query("Edition");
 		editionsQuery.equalTo("editionName", req.params.name);
-		editionsQuery.first().then(function(edition) {
+		editionsQuery.first({ sessionToken: token }).then(function(edition) {
 			res.success(edition ? false : true);
 		}, function(error) {
 			res.error(error);
