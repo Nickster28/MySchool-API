2713e84b734a8a398d1202de58882b8520501228
Nick Troccoli <nicktroccoli28@gmail.com>
2016-08-12 00:20:19 -0400

    Added comments, modified editions name checker to also check empty string

--- cloud/editions.js
+++ cloud/editions.js
@@ -1,9 +1,26 @@
+/*
+ * CLOUD FUNCTION: IsValidEditionName
+ * ------------------------------------
+ * Parameters:
+ *		name - the edition name to validate
+ *
+ * Returns: whether or not this edition name is valid (non-empty, unique).
+ * ------------------------------------
+ */
 Parse.Cloud.define("IsValidEditionName", function(req, res) {
-	var editionsQuery = new Parse.Query("Edition");
-	editionsQuery.equalTo("editionName", req.params.name);
-	editionsQuery.first().then(function(edition) {
-		res.success(edition ? false : true);
-	}, function(error) {
-		res.error(error);
-	});
+	
+	// If there's no name, send an error, and if the name is empty, send false
+	if (!req.params.name) {
+		res.error("No name provided");
+	else if (req.params.name === "") {
+		res.success(false);
+	} else {
+		var editionsQuery = new Parse.Query("Edition");
+		editionsQuery.equalTo("editionName", req.params.name);
+		editionsQuery.first().then(function(edition) {
+			res.success(edition ? false : true);
+		}, function(error) {
+			res.error(error);
+		});
+	}
 });
