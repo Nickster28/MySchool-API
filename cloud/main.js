5c6dbbf0043307d0f9a93d5b401784d12e950f9d
Nick Troccoli <nicktroccoli28@gmail.com>
2016-08-14 21:29:23 -0400

    Updated naming from edition to newspaperedition

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,5 @@
 require('./authentication.js');
-require('./editions.js');
+require('./newspaperEditions.js');
 
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
--- cloud/newspaperEditions.js
+++ cloud/newspaperEditions.js
@@ -1,5 +1,5 @@
 /*
- * FUNCTION: isValidEditionName
+ * FUNCTION: isValidNewspaperEditionName
  * -----------------------------
  * Parameters:
  *		name - the edition name to validate
@@ -11,14 +11,15 @@
  * 			validate the query.
  * -----------------------------
  */
-function isValidEditionName(name, sessionToken) {
+function isValidNewspaperEditionName(name, sessionToken) {
 	if (name === undefined || name === null) {
 		return Parse.Promise.error("No name provided");
 	} else if (name === "") {
 		return Parse.Promise.as(false);
 	} else {
 		var queryOptions = sessionToken ? {sessionToken: sessionToken} : {};
-		var editionsQuery = new Parse.Query(Parse.Object.extend("Edition"));
+		var NewspaperEdition = Parse.Object.extend("NewspaperEdition");
+		var editionsQuery = new Parse.Query(NewspaperEdition);
 		editionsQuery.equalTo("editionName", name);
 		return editionsQuery.first(queryOptions).then(function(edition) {
 			return (edition ? false : true);
@@ -27,7 +28,7 @@ function isValidEditionName(name, sessionToken) {
 }
 
 /*
- * CLOUD FUNCTION: IsValidEditionName
+ * CLOUD FUNCTION: IsValidNewspaperEditionName
  * ------------------------------------
  * Request Parameters:
  *		name - the edition name to validate
@@ -37,12 +38,12 @@ function isValidEditionName(name, sessionToken) {
  * Must be called by a logged-in Parse User.
  * ------------------------------------
  */
-Parse.Cloud.define("IsValidEditionName", function(req, res) {
+Parse.Cloud.define("IsValidNewspaperEditionName", function(req, res) {
 	if (!req.user) {
 		res.error("Request must be made by logged-in user");
 	} else {
 		var name = req.params.name;
-		return isValidEditionName(name, req.user.getSessionToken())
+		return isValidNewspaperEditionName(name, req.user.getSessionToken())
 		.then(function(isValid) {
 			res.success(isValid);
 		}, function(error) {
@@ -52,19 +53,19 @@ Parse.Cloud.define("IsValidEditionName", function(req, res) {
 });
 
 /*
- * CLOUD FUNCTION: BEFORESAVE EDITION
+ * CLOUD FUNCTION: BeforeSave NewspaperEdition
  * -----------------------------------
  * Verifies that edition names are unique before saving a new object.
  * -----------------------------------
  */
-Parse.Cloud.beforeSave("Edition", function(req, res) {
+Parse.Cloud.beforeSave("NewspaperEdition", function(req, res) {
 	if (req.object.isNew()) {
 		var name = req.object.get("editionName");
-		return isValidEditionName(name).then(function(isValid) {
+		return isValidNewspaperEditionName(name).then(function(isValid) {
 			if (isValid) {
 				res.success();
 			} else {
-				res.error("Edition name is not unique");
+				res.error("Newspaper Edition name is not unique");
 			}
 		}, function(error) {
 			res.error(error);
