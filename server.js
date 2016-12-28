4371e7395ca15a8003c7218b59f18773bc593573
Nick Troccoli <nicktroccoli28@gmail.com>
2016-12-28 17:02:42 -0500

    Updated server.js to also mount dashboard at /dashboard

--- server.js
+++ server.js
@@ -1,40 +1,41 @@
-var express = require('express');
-var ParseServer = require('parse-server').ParseServer;
-var ParseDashboard = require('parse-dashboard');
-var path = require('path');
+var express = require("express");
+var ParseDashboard = require("parse-dashboard");
+var ParseServer = require("parse-server").ParseServer;
+var path = require("path");
 
-// Serve the Parse API on the /parse URL prefix
-var mountPath = process.env.PARSE_MOUNT || '/parse';
+if (!process.env.MONGODB_URI || !process.env.APP_ID || !process.env.MASTER_KEY
+	|| process.env.PARSE_DASHBOARD_CONFIG) {
+	console.log("Error: one or more fields missing");
+}
 
 var api = new ParseServer({
-  	databaseURI: process.env.MONGODB_URI || '',
-  	cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  	appId: process.env.APP_ID || '',
-  	masterKey: process.env.MASTER_KEY || '',
-  	serverURL: (process.env.SERVER_URL || 'http://localhost:1337') + mountPath
+  databaseURI: process.env.MONGODB_URI,
+  cloud: __dirname + '/cloud/main.js',
+  appId: process.env.APP_ID,
+  masterKey: process.env.MASTER_KEY,
+  serverURL: process.env.SERVER_URL || "http://localhost:1337/parse",
+  liveQuery: {
+    classNames: [] // List of classes to support for query subscriptions
+  }
 });
 
-var dashboardSettings = JSON.parse(process.env.PARSE_DASHBOARD_CONFIG);
-var dashboard = new ParseDashboard(dashboardSettings);
+var dashboardConfig = JSON.parse(process.env.PARSE_DASHBOARD_CONFIG);
+var dashboard = new ParseDashboard();
 
 var app = express();
 
-// Serve static assets from the /public folder
-app.use('/public', express.static(path.join(__dirname, '/public')));
-
-app.use(mountPath, api);
-
+app.use('/parse', api);
 app.use('/dashboard', dashboard);
 
-// Parse Server plays nicely with the rest of your web routes
-app.get('/', function(req, res) {
-  	res.status(200).send('Hello world!');
+app.get('*', function(req, res) {
+  res.status(200).send('Hello world!');
 });
-
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
-  	console.log('API running on port ' + port + '.');
+    console.log('parse-server-example running on port ' + port + '.');
 });
 
+// This will enable the Live Query real-time server
+ParseServer.createLiveQueryServer(httpServer);
