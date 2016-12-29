46ea801ce000c9a10cc5446d2a792c764638c194
Nick Troccoli <nicktroccoli28@gmail.com>
2016-12-28 22:05:02 -0500

    Added starter code for calendars scheduled job

--- updateCalendars.js
+++ updateCalendars.js
@@ -1,1 +1,28 @@
-console.log("Hello world!");
+const Parse = require("parse/node");
+
+
+function updateCalendars(serverURL) {
+	updateSchoolCalendar(serverURL).then(function() {
+		return updateAthleticsCalendar(serverURL);
+	}).then(function() {
+		console.log("Calendars updated!");
+	}, function(error) {
+		console.log("An error occurred: " + JSON.stringify(error));
+	});
+}
+
+
+function updateSchoolCalendar(serverURL) {
+	return Parse.Promise.as(true);
+}
+
+
+function updateAthleticsCalendar(serverURL) {
+	return Parse.Promise.as(true);
+}
+
+
+Parse.initialize(process.env.APP_ID);
+Parse.serverURL = process.env.SERVER_URL + "/parse";
+const calendarServerURL = process.env.CALENDAR_SERVER_URL;
+updateCalendars(calendarServerURL);
