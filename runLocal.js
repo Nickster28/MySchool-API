06d1dc17d9551c7b6ab3b6f26f384fc12e1628f1
Nick Troccoli <nicktroccoli28@gmail.com>
2016-12-29 20:18:06 -0500

    Implemented updateCalendar script part for school calendar

--- runLocal.js
+++ runLocal.js
@@ -14,12 +14,14 @@ runLocal.json file with the server info in the following format:
 
 where "configs" maps to a dictionary of server names to server config objects,
 and "dashboard" maps to a Parse Dashboard config object as a JSON string.
-Each config object must have an "appId", "masterKey", and "mongoUri" field.
+Each config object must have an "appId", "masterKey", "calendarServerUrl" and
+"mongoUri" field.
 
 With this companion file, this script takes 1-2 command line arguments:
 
-	node runLocal.js SERVER_NAME DEBUG_OPTION
+	node runLocal.js FILE_NAME SERVER_NAME DEBUG_OPTION
 
+- (REQUIRED) FILE_NAME - either "server.js" or "updateCalendars.js" to run
 - (REQUIRED) SERVER_NAME - must match a configuration object in "configs" above.
 Runs that server configuration.
 - (OPTIONAL) DEBUG_OPTION - if "debug", runs using node-debug instead of the
@@ -33,32 +35,37 @@ var serverInfo = JSON.parse(fs.readFileSync("runLocal.json", "utf8"));
 
 
 /* FUNCTION: argsStringForServerName
- * ----------------------------------------
- * Parameters:
- * 		serverName - name of the server to create a string for
- *
- * Returns: the command line string needed to start parse server.
- * 		Format: "APP_ID={APP_ID} MASTER_KEY={MASTER_KEY} MONGODB_URI={URI} DASHBOARD_CONFIG={JSON_STRING}"
- * ----------------------------------------
- */
+----------------------------------------
+	Parameters:
+		serverName - name of the server to create a string for
+
+Returns: the command line string needed to start parse server.
+ 		Format: "APP_ID={APP_ID} MASTER_KEY={MASTER_KEY} MONGODB_URI={URI} 
+				CALENDAR_SERVER_URL={URL} DASHBOARD_CONFIG={JSON_STRING}"
+----------------------------------------
+*/
 function argsStringForServerName(serverName) {
 	var selectedServerInfo = serverInfo.configs[serverName];
 	if (!selectedServerInfo) return null;
 
 	return 'APP_ID=\"' + selectedServerInfo.appId + '\" MASTER_KEY=\"' +
 		selectedServerInfo.masterKey + '\" MONGODB_URI=\"' +
-		selectedServerInfo.mongoUri + '\"' + '\" DASHBOARD_CONFIG=\"' +
-		JSON.toString(serverInfo.dashboard) + '\"';
+		selectedServerInfo.mongoUri + '\" CALENDAR_SERVER_URL=\"' + 
+		selectedServerInfo.calendarServerUrl + '\" DASHBOARD_CONFIG=\'' +
+		JSON.stringify(serverInfo.dashboard) + '\'';
 }
 
 
 // Ignore the first 2 'node' and process name args
 var args = process.argv.slice(2)
 
-var argsString = serverArgsStringForServerName(args[0]);
+var argsString = argsStringForServerName(args[1]);
 if (!argsString) {
 	console.error("Error: invalid server name");
 } else {
-	var command = args[1] == "debug" ? "node-debug" : "nodemon";
-	execSync(argsString + " " + command + " " + "server.js", {stdio:[0,1,2]});
+	var command = args[2] == "debug" ? "node-debug" : "nodemon";
+	if (args[0] == "updateCalendars.js" && command == "nodemon") {
+		command = "node";
+	}
+	execSync(argsString + " " + command + " " + args[0], {stdio:[0,1,2]});
 }
--- updateCalendars.js
+++ updateCalendars.js
@@ -1,6 +1,20 @@
 const Parse = require("parse/node");
+const request = require("request");
 
 
+/* FUNCTION: updateCalendars
+-----------------------------
+Parameters:
+	serverURL - the URL to request calendar data from.
+
+Returns: a promise that sends requests to the given URL to update our school
+calendar and athletics calendar data on Parse.  For the school calendar, we
+replace all existing data with the updated data.  For the athletics calendar,
+we do a diff on existing events and, if we see an updat, or if an event is
+removed, we send a push notification to anyone who's subscribed to that team's
+channel on Parse.
+-----------------------------
+*/
 function updateCalendars(serverURL) {
 	updateSchoolCalendar(serverURL).then(function() {
 		return updateAthleticsCalendar(serverURL);
@@ -12,17 +26,93 @@ function updateCalendars(serverURL) {
 }
 
 
+/* FUNCTION: updateSchoolCalendar
+----------------------------------
+Parameters:
+	serverURL - the URL to request school calendar data from.
+
+Returns: a promise that sends a request to the given URL to update our school
+calendar data on Parse.  We replace all existing school calendar data with the
+updated data.
+----------------------------------
+*/
 function updateSchoolCalendar(serverURL) {
-	return Parse.Promise.as(true);
+	return getURL(serverURL + "/schoolCalendar").then(function(responseBody) {
+		return JSON.parse(responseBody);
+	}).then(function(calendarData) {
+		const oldCalendarQuery = new Parse.Query("CalendarEvent");
+		oldCalendarQuery.limit(1000);
+		return oldCalendarQuery.find().then(function(oldCalendarEvents) {
+			return Parse.Object.destroyAll(oldCalendarEvents);
+		}).then(function() {
+			return createNewCalendarEvents(calendarData);
+		});
+	});
 }
 
 
+/* FUNCTION: getURL
+--------------------------
+Parameters:
+    url - the url to GET
+
+Returns: a promise containing the GET response from the given url
+
+Uses "request" within a promise.  If there's an error, the
+error will be passed back in a promise.  Otherwise, the response
+is passed back.
+--------------------------
+*/
+function getURL(url) {
+    "use strict";
+    return new Promise(function(resolve, reject) {
+        request(url, function(error, response, body) {
+            if(error) reject(error);
+            else resolve(body);
+        });
+    });
+}
+
+
+/* FUNCTION: createNewCalendarEvents
+-------------------------------------
+Parameters:
+	calendarData - an array of calendar objects to make into Parse CalendarEvent
+					objects.
+
+Returns: a promise that saves new CalendarEvent objects for each element in
+the calendarData array.  Saves all objects to the server in parallel.
+-------------------------------------
+*/
+function createNewCalendarEvents(calendarData) {
+	const CalendarEvent = Parse.Object.extend("CalendarEvent");
+
+	return Parse.Promise.when(calendarData.map(function(eventData) {
+		// Create a new CalendarEvent Parse object
+		const calendarEvent = new CalendarEvent();
+		calendarEvent.set("eventName", eventData.eventName);
+		calendarEvent.set("startDateTime", new Date(eventData.startDateTime));
+		if (eventData.endDateTime) {
+			calendarEvent.set("endDateTime", new Date(eventData.endDateTime));
+		}
+		if (eventData.location) {
+			calendarEvent.set("location", eventData.location);
+		}
+
+		return calendarEvent.save();
+	}));
+}
+
+
+// TODO
 function updateAthleticsCalendar(serverURL) {
 	return Parse.Promise.as(true);
 }
 
 
+if (!process.env.SERVER_URL) process.env.SERVER_URL = "http://localhost:1337";
 Parse.initialize(process.env.APP_ID);
 Parse.serverURL = process.env.SERVER_URL + "/parse";
 const calendarServerURL = process.env.CALENDAR_SERVER_URL;
 updateCalendars(calendarServerURL);
+
